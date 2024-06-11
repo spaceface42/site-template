282ce97f09b261739e79d27845622c092050c9a1
Zsolt Sándor <ezsolt@gmail.com>
2024-06-11 17:52:54 +0200

    using promisedom.js now

--- docs/starters/DarkAndLight_NEW.js
+++ docs/starters/DarkAndLight_NEW.js
@@ -1,4 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
+/**
+ * init.ts
+ *
+ * Initialization script for the application.
+ */
+import PromiseDom from '../rsrc/42/PromiseDom.js';
+// import FetchPartial from '../42/FetchPartial.js';
+
+async function start() {
+    // Instantiate PromiseDom
+    const domReady = new PromiseDom();
+    try {
+        await domReady.ready;
+        console.log('app.start | DOM is fully loaded and parsed');
+
+        // html message
+        // Create an instance of FetchPartial
+        // const fetchPartial = new FetchPartial();
+        // Fetch and process all partial HTML content
+        // await fetchPartial.fetchAll();
+        // console.log('app.start | All partial HTML content fetched and processed');
+
+      setup();
+
+    }
+    catch (error) {
+        console.error('app.start | Error during initialization:', error);
+    }
+}
+
+// Start the script
+start();
+
+
+
+
+function setup() {
 
 
 
@@ -116,4 +152,4 @@ function getLocalStorageItem(key) {
 
 
 
-});
+}
