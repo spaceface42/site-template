ac0d98cba4aa2f796497667105a96731d772324a
Zsolt Sándor <ezsolt@gmail.com>
2024-06-13 08:13:22 +0200

    fixing paths from starters folder

--- docs/starters/partials.js
+++ docs/starters/partials.js
@@ -4,20 +4,24 @@
  * Initialization script for the application.
  */
 import PromiseDom from '../rsrc/42/PromiseDom.js';
-// import FetchPartial from '../42/FetchPartial.js';
-
+import FetchPartial from '../rsrc/42/FetchPartial.js';
 async function start() {
     // Instantiate PromiseDom
     const domReady = new PromiseDom();
     try {
         await domReady.ready;
         console.log('app.start | DOM is fully loaded and parsed');
-
+        // html message
+        const appElement = document.getElementById('app');
+        if (appElement) {
+            appElement.innerHTML = '<h3>Welcome to version 1.0.0</h3>';
+        }
+        // html message
         // Create an instance of FetchPartial
-        // const fetchPartial = new FetchPartial();
+        const fetchPartial = new FetchPartial();
         // Fetch and process all partial HTML content
-        // await fetchPartial.fetchAll();
-        // console.log('app.start | All partial HTML content fetched and processed');
+        await fetchPartial.fetchAll();
+        console.log('app.start | All partial HTML content fetched and processed');
     }
     catch (error) {
         console.error('app.start | Error during initialization:', error);
