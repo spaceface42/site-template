6c2e2cb09b83c933bbfd9611d5e86aaa5b265b57
Zsolt Sándor <ezsolt@gmail.com>
2024-07-04 10:47:53 +0200

    delete

--- docs/rsrc/42/DomUpdater.d.ts
+++
@@ -1,8 +0,0 @@
-declare class DomUpdater {
-    private fetchPartial;
-    constructor();
-    updateAllPartials(selector?: string): Promise<void>;
-    private updatePartial;
-    private insertContent;
-}
-export default DomUpdater;
--- docs/rsrc/app/config.js
+++
@@ -1,2 +0,0 @@
-export const APP_VERSION = '1.2.0';
-//# sourceMappingURL=config.js.map
